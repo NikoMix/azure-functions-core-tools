5f535b2489688ccc3ac50a5bf6712d0f9b067c73
gzuber <grahamzuber@gmail.com>
2020-08-05 12:55:16 -0700

    Added support for user secrets file.

--- src/Azure.Functions.Cli/Common/Constants.cs
+++ src/Azure.Functions.Cli/Common/Constants.cs
@@ -50,6 +50,7 @@ namespace Azure.Functions.Cli.Common
         public const string OSXRootPath = "~/";
         public const string ManagedDependencyConfigPropertyName = "managedDependency";
         public const string PowerShellWorkerDefaultVersion = "~7";
+        public const string UserSecretsIdElementName = "UserSecretsId";
 
         public static string CliVersion => typeof(Constants).GetTypeInfo().Assembly.GetName().Version.ToString(3);
 
---
+++ src/Azure.Functions.Cli/Secrets/UserSecretsConfigurationBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Azure.Functions.Cli.Common;
+using Azure.Functions.Cli.Helpers;
+using Microsoft.Azure.WebJobs.Script;
+using Microsoft.Extensions.Configuration;
+
+namespace Azure.Functions.Cli.Diagnostics
+{
+    internal class UserSecretsConfigurationBuilder : IConfigureBuilder<IConfigurationBuilder>
+    {
+        private readonly string _scriptPath;
+
+        public UserSecretsConfigurationBuilder(string scriptPath)
+        {
+            _scriptPath = scriptPath;
+        }
+
+        public void Configure(IConfigurationBuilder builder)
+        {
+            string userSecretsId = GetUserSecretsId(_scriptPath);
+            if (userSecretsId == null) return;
+
+            builder.AddUserSecrets(userSecretsId);
+        }
+
+        private string GetUserSecretsId(string scriptPath)
+        {
+            string projectFilePath = ProjectHelpers.FindProjectFile(scriptPath);
+            if (projectFilePath == null) return null;
+
+            var projectRoot = ProjectHelpers.GetProject(projectFilePath);
+            var userSecretsId = ProjectHelpers.GetPropertyValue(projectRoot, Constants.UserSecretsIdElementName);
+
+            return userSecretsId;
+        }
+    }
+}
